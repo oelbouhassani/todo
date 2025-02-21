475d51ab3c7d8f212324f1892d57ce69299201b7
omar el bouhassani <elbouhassaniomar@gmail.com>
2025-02-21 18:49:28 +0100

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { FormControl } from '@angular/forms';  // Import FormControl
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'todo-list-angular';
+export class AppComponent implements OnInit {
+  todos: any[] = [];
+
+  newTodo = new FormControl('');
+  apiUrl = 'http://localhost:8080/todos';
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.loadTodos();
+  }
+
+  loadTodos() {
+    this.http.get<any[]>(this.apiUrl).subscribe(data => this.todos = data);
+  }
+
+  addTodo() {
+  console.log("newTodo bbbb=>", this.newTodo);
+    this.http.post<any>(this.apiUrl, { title: this.newTodo.value, completed: false }).subscribe(todo => {
+      this.todos.push(todo);
+    });
+  }
+
+  deleteTodo(id: number) {
+    this.http.delete(`${this.apiUrl}/${id}`).subscribe(() => {
+      this.todos = this.todos.filter(todo => todo.id !== id);
+    });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
-
+import { AppRoutingModule } from './app-routing.module'
+import { ReactiveFormsModule } from '@angular/forms';  // Import ReactiveFormsModule
+;
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
